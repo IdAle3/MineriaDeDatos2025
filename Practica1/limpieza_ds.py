1b36c89b647311c7a433fc3aa21725458cc44e44
IdAle3 <idalia.delgadom@uanl.edu.mx>
2025-09-03 21:07:27 -0600

    Practica 1

--- Practica1/limpieza_ds.py
+++ Practica1/limpieza_ds.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 # 1. Cargar dataset original de Chicago Crimes
-df = pd.read_csv("Chicago_Crimes_2012_to_2017.csv", low_memory=False)
+df = pd.read_csv("Crimenes_Chicago_Resumido.csv", low_memory=False)
 print("Shape inicial:", df.shape)  # Muestra tamaño original
 
 # 2. Selección de columnas relevantes para la práctica
